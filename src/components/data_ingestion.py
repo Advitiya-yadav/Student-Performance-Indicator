e8f65afa60bcab7091c3f8da359070816a4e5db2
Advitiya Yadav <advitiyayadav2105@gmail.com>
2025-12-27 19:59:13 +0530

    Completed the Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging # to use logging from the logger file in src
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 from src.components.data_transformation import DataTransformationConfig
 import pandas as pd
 
@@ -53,4 +54,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
