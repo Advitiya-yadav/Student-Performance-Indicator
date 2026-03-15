20bfa95acfdf0b0dc35bf6a2406be0324c856029
Advitiya Yadav <advitiyayadav2105@gmail.com>
2026-03-16 01:27:58 +0530

    Pipeline fixes

--- predict_pipeline.py
+++ predict_pipeline.py
@@ -2,33 +2,25 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import joblib
 from pathlib import Path
 
-root_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(root_dir, 'src')
-if src_dir not in sys.path:
-    sys.path.insert(0, src_dir)
-
-from exception import CustomException
-from logger import logging
-from utils import save_object, load_object
+def load_object(file_path):
+    return joblib.load(file_path)
 
 class PredictPipeline:
     def __init__(self):
         pass
 
     def predict(self, features):
-        try:
-            root = Path.cwd()
-            model_path = root / "artifacts" / "model.pkl"
-            preprocessor_path = root / "artifacts" / "preprocessor.pkl"
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
-            scaled_data = preprocessor.transform(features)
-            prediction = model.predict(scaled_data)
-            return prediction
-        except Exception as e:
-            raise CustomException(e, sys)
+        root = Path(__file__).parent
+        model_path = root / "artifacts" / "model.pkl"
+        preprocessor_path = root / "artifacts" / "preprocessor.pkl"
+        model = load_object(model_path)
+        preprocessor = load_object(preprocessor_path)
+        scaled_data = preprocessor.transform(features)
+        prediction = model.predict(scaled_data)
+        return prediction
 
 class CustomData:
     def __init__(self, gender, race_ethnicity, parental_level_of_education, lunch, test_preparation_course, reading_score, writing_score):
@@ -41,16 +33,12 @@ class CustomData:
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
-        try:
-            custom_data_input_dict = {
-                'gender': [self.gender],
-                'race_ethnicity': [self.race_ethnicity],
-                'parental_level_of_education': [self.parental_level_of_education],
-                'lunch': [self.lunch],
-                'test_preparation_course': [self.test_preparation_course],
-                'reading_score': [self.reading_score],
-                'writing_score': [self.writing_score]
-            }
-            return pd.DataFrame(custom_data_input_dict)
-        except Exception as e:
-            raise CustomException(e, sys)
+        return pd.DataFrame({
+            'gender': [self.gender],
+            'race_ethnicity': [self.race_ethnicity],
+            'parental_level_of_education': [self.parental_level_of_education],
+            'lunch': [self.lunch],
+            'test_preparation_course': [self.test_preparation_course],
+            'reading_score': [self.reading_score],
+            'writing_score': [self.writing_score]
+        })
